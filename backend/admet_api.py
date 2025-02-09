446d1d34de019bcfbedda31b58f25fd623154789
Aritra-Script <aritrachakraborty001@outlook.com>
2025-02-09 18:12:32 +0530

    Added comment in admet_api.py

--- backend/admet_api.py
+++ backend/admet_api.py
@@ -21,7 +21,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
+    allow_origins=["http://localhost:5173"], #Add your url here for frontend
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
