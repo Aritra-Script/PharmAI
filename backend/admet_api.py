84212e7ba67606f68a94373271e733c4e5cdcf6a
Aritra-Script <aritrachakraborty001@outlook.com>
2025-04-06 12:20:12 +0530

    2.2.2 dev

--- backend/admet_api.py
+++ backend/admet_api.py
@@ -21,7 +21,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], #Add your url here for frontend
+    allow_origins=["http://localhost:5173","https://aritra-script.github.io/PharmAI/"], #Add your url here for frontend dev url
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
